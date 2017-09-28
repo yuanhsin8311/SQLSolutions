98598a76565fab208e279473effbe36b74cb1e37
yuanhsin huang <yuanhsin8311@gmail.com>
2017-09-28 13:49:30 -0700

    deleting duplicate files

--- RevisingtheSelectQueryII.sql
+++
@@ -1,9 +0,0 @@
--- Query the names of all American cities in CITY with populations larger than 120000. The CountryCode for America is USA.
-
--- Input Format
-
--- The CITY table is described as follows:
-
-select name
-from city
-where population > 120000 and countrycode = "usa";
