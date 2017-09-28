b0c60c498e38d87880f29c3cd5063e2c4785897b
yuanhsin huang <yuanhsin8311@gmail.com>
2017-09-28 13:43:35 -0700

    first commit

---
+++ RevisingtheSelectQueryI.sql
@@ -0,0 +1,10 @@
+Query all columns for all American cities in CITY with populations larger than 100000. The CountryCode for America is USA.
+
+Input Format
+
+The CITY table is described as follows: CITY.jpg
+
+select *
+from city
+where population > 100000
+and countrycode="usa";
