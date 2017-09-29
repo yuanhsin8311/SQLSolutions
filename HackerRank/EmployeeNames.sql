46b558ef7e966a36a951f1f9c3c70837e377ed97
yuanhsin huang <yuanhsin8311@gmail.com>
2017-09-29 13:20:19 -0700

    add  files

--- HackerRank/EmployeeNames.sql
+++ HackerRank/EmployeeNames.sql
@@ -1,23 +1,27 @@
--- Query the Name of any student in STUDENTS who scored higher than 75 Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+-- Write a query that prints a list of employee names 
+-- (i.e.: the name attribute) from the Employee table in alphabetical order.
 
 -- Input Format
 
--- The STUDENTS table is described as follows:
+-- The Employee table containing employee data for a company is described as follows:
 
--- The Name column only contains uppercase (A-Z) and lowercase (a-z) letters.
+-- where employee_id is an employee's ID number, name is their name, months is the total number of months they've been working for the company, and salary is their monthly salary.
 
 -- Sample Input
 
 -- Sample Output
 
--- Ashley
--- Julia
--- Belvet
--- Explanation
+-- Angela
+-- Bonnie
+-- Frank
+-- Joe
+-- Kimberly
+-- Lisa
+-- Michael
+-- Patrick
+-- Rose
+-- Todd
 
--- Only Ashley, Julia, and Belvet have Marks > . If you look at the last three characters of each of their names, there are no duplicates and 'ley' < 'lia' < 'vet'.
-
-select name 
-from students
-where marks > 75
-order by right(name,3),ID ; 
+select name
+from employee
+order by name;
